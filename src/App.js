410fc02b1a93a85a3563b12db2c0c80f519e10c3
avizmarques <aviz.marques@gmail.com>
2020-01-20 12:01:44 +0100

    changed page content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello this is Alex OVER</h1>
+      <p>Hello there from the app side</p>
+      <p>JSX is freaking weird</p>
     </div>
   );
 }
