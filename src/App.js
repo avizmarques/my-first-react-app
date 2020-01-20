627928960d78e724d2e117551582b4f2ddc67566
avizmarques <aviz.marques@gmail.com>
2020-01-20 12:07:21 +0100

    changed heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello this is Alex OVER</h1>
+      <h1>Welcome to Codaisseur</h1>
       <p>Hello there from the app side</p>
       <p>JSX is freaking weird</p>
     </div>
